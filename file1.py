a82507cc3a2097c0983f350d30410d242ea5bdc7
yaron chen <yaron.chen68@gmail.com>
2023-12-08 08:51:20 +0200

    update my_feature 4

--- file1.py
+++ file1.py
@@ -1,46 +1,54 @@
-class Person:
-     def __init__(self, name='', age=0):
-        self.__name = name
-        self.__age = age
-     def say(self):
-        return "Hi :)"
-     def __str__(self):
-        return "Person {} is {} years old".\
-            format(self.__name, self.__age)
-     def get_name(self):
-        return self.__name
-     def get_age(self):
-        return self.__age
-     def set_name(self, name):
-        self.__name = name
-     def set_age(self, age):
-        self.__age = age
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
+search_word = sys.argv[1]
+# Assuming you have already initialized your WebDriver (e.g., webdriver.Chrome())
+driver = webdriver.Chrome()
+url = "https://elbitsystemscareer.com/search-results/?keywords="
+places = ['שרון' ,'גוש דן']
+for place in places:
+    driver.get(url)
+    select_element = driver.find_element(By.ID, 'select')
 
-class Student(Person):
-    def __init__(self, name, age, avrege_degre=0):
-        super(Student, self).__init__(name, age)
+    # Use the Select class to interact with the dropdown
+    select = Select(select_element)
 
-        self.__avrege_degre = avrege_degre
+    select.select_by_visible_text(place)
+    button = driver.find_element(By.XPATH, '//button[@aria-label="חפש לפי איזור"]')
 
-    def    get_avrege_degre(self):
-        return self.__avrege_degre
+    # Click the button
+    button.click()
+    end = True
+    index = 1
+    while(end):
+        # Locate the text search_word" using XPath
+        try:
+            xpath_search = f"//label[a[contains(text(), '{search_word}')]]"
+            job_description = driver.find_element(By.XPATH, xpath_search)
+        except:
+            # Locate the "הבא" link by its class name
+            try:
+                next_button = driver.find_element(By.CLASS_NAME, 'next')
 
-    def    set_avrege_degre(self, avrege_degre):
-        self.__avrege_degre = avrege_degre
+                # Click the "הבא" link
+                next_button.click()
+                index += 1
+                continue
+            except:
+                break
+        # Get the text content of the label
+        job_description_text = job_description.text
+        print(f"aria {place} page {index} : {job_description_text}")
+        try:
+            next_button = driver.find_element(By.CLASS_NAME, 'next')
 
-    def __str__(self):
-        return f"student name  {self.get_name()} is {self.get_age()} years old and his " \
-               f"avrege_degre {self.__avrege_degre}"
+            # Click the "הבא" link
+            next_button.click()
+            index += 1
+        except:
+            break
 
-    def say(self):
-        return "Hi everybody:)"
-
-class CyberStudent(Student):
-    def __init__(self, name, age, avrege_degre, cyber_grade):
-        super(CyberStudent, self).__init__(name, age, avrege_degre)
-
-        self.__cyber_grade = cyber_grade
-
-    def get_cyber_grade(self):
-        return self.__cyber_grade
+# Close the browser window
+driver.quit()
